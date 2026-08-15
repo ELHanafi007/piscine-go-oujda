e4a87db54d7cb3ceaf6b74f94cadd254b3949a05
eabbaali <eabbaali@gmail.com>
2026-08-15 06:01:58 +0100

    seriousssssss

--- basicatoi2.go
+++ basicatoi2.go
@@ -13,10 +13,3 @@ func BasicAtoi2(s string) int {
 
 	return result
 }
-
-/*func main() {
-	fmt.Println(BasicAtoi2("12345"))
-	fmt.Println(BasicAtoi2("0000000012345"))
-	fmt.Println(BasicAtoi2("012 345"))
-	fmt.Println(BasicAtoi2("Hello World!"))
-}*/
