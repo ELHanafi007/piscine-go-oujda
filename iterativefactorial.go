e7f16cd22495d6971993db078866413dba0042cc
eabbaali <elhanafiabbaali@gmail.com>
2026-08-17 18:50:23 +0100

    first commit of the day for quest4; solving the iterative funcccc

---
+++ iterativefactorial.go
@@ -0,0 +1,27 @@
+package piscine
+
+/*
+import (
+
+	"fmt"
+
+)
+*/
+func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
+	r := 1
+
+	for i := 1; i <= nb; i++ {
+		r *= i
+	}
+
+	return r
+}
+
+/*func main() {
+	arg := 4
+	fmt.Println(IterativeFactorial(arg))
+}*/
