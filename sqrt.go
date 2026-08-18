b93f9acdd6f4b9f56b0010c3310addc7e0a94156
eabbaali <elhanafiabbaali@gmail.com>
2026-08-18 12:36:55 +0100

    fff

--- sqrt.go
+++ sqrt.go
@@ -1,10 +1,23 @@
-package piscine
+package main
+
+import (
+	"fmt"
+)
 
 func Sqrt(nb int) int {
+	if nb == 1 {
+		return 1
+	}
 	for i := 1; i < nb; i++ {
 		if nb == i*i {
 			return i
 		}
 	}
 	return 0
+}
+
+func main() {
+	fmt.Println(Sqrt(4))
+	fmt.Println(Sqrt(3))
+	fmt.Println(Sqrt(1))
 }
