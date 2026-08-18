f95b92289b54ccd32a654116577cb05ade5c2ad7
eabbaali <elhanafiabbaali@gmail.com>
2026-08-18 23:55:32 +0100

    message

---
+++ findnextprime.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func FindNextPrime(nb int) int {
+	for !IsPrime(nb) {
+		nb++
+	}
+	return nb
+}
+
+func IsPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	}
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(FindNextPrime(5))
+	fmt.Println(FindNextPrime(4))
+}
