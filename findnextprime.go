28001b925a74d4d408a5f60f2c1db463c0933b16
eabbaali <elhanafiabbaali@gmail.com>
2026-08-19 02:27:25 +0100

    SOS somebody rescue me

--- findnextprime.go
+++ findnextprime.go
@@ -4,17 +4,6 @@ func FindNextPrime(nb int) int {
 	for !IsPrime(nb) {
 		nb++
 	}
-	return nb
-}
 
-func IsPrime(nb int) bool {
-	if nb <= 1 {
-		return false
-	}
-	for i := 2; i*i <= nb; i++ {
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return true
+	return nb
 }
