42e90712747bee490c537880b608762cff7f50a1
eabbaali <elhanafiabbaali@gmail.com>
2026-08-21 18:40:32 +0100

    firstt

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
-	for _, r := range os.Args[0] {
+	arg := os.Args[0]
+	for i := len(arg) - 1; i >= 0; i-- {
+		if arg[i] == '/' {
+			arg = arg[i+1:]
+			break
+		}
+	}
+	for _, r := range arg {
 		z01.PrintRune(r)
 	}
-	z01.PrintRune('\n')
 }
