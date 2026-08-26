1caca6bf107c5ec9d82eaccc7f973229741da39c
eabbaali <elhanafiabbaali@gmail.com>
2026-08-26 12:12:46 +0100

    easy boy

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -1,18 +1,18 @@
 package piscine
 
 type food struct {
-  preptime int
+	preptime int
 }
 
 func FoodDeliveryTime(order string) int {
 	orderT := 0
-	if order == "burger"{
+	if order == "burger" {
 		orderT += 15
 	}
-	if order == "chips"{
+	if order == "chips" {
 		orderT += 10
 	}
-	if order == "nuggets"{
+	if order == "nuggets" {
 		orderT += 12
 	}
 	return orderT
--- rot14.go
+++ rot14.go
@@ -1,14 +1,14 @@
 package piscine
 
 func Rot14(s string) string {
-  str := ""
-  for _, r := range s{
-    if r >= 'a' && r <= 'z'{
-      r = (r-'a'+14)%26 + 'a'
-    }else if r >= 'A' && r <= 'Z'{
-      r = (r-'A'+14)%26 + 'A'
-    }
-  str += string(r)
-  }
-  return str
+	str := ""
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' {
+			r = (r-'a'+14)%26 + 'a'
+		} else if r >= 'A' && r <= 'Z' {
+			r = (r-'A'+14)%26 + 'A'
+		}
+		str += string(r)
+	}
+	return str
 }
