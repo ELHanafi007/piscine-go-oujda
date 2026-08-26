4eec1df8b532662171c89372c93819a973922a21
eabbaali <elhanafiabbaali@gmail.com>
2026-08-26 15:44:46 +0100

    ff

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,26 @@
+package piscine
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	list := make(map[string]int)
+	currW := ""
+	for _, r := range str {
+		if isalpha(r) {
+			currW += string(r)
+		} else if r == ' ' {
+			list[currW]++
+			currW = ""
+		} else if currW != "" {
+			list[currW]++
+		}
+	}
+	return list
+}
+
+func isalpha(r rune) bool {
+	if r >= 'a' && r <= 'z' {
+		return true
+	} else if r >= 'A' && r <= 'Z' {
+		return true
+	}
+	return false
+}
