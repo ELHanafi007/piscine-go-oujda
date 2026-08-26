75afa49d492d7c1b250f89b4c1460eaf6c0628b1
eabbaali <elhanafiabbaali@gmail.com>
2026-08-27 00:23:49 +0100

    addig the comeback

--- comcheck/main.go
+++ comcheck/main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-	for i := 0; i < len(os.Args); i++ {
+	for i := 1; i < len(os.Args); i++ {
 		if os.Args[i] == "galaxy 01" || os.Args[i] == "galaxy" || os.Args[i] == "01" {
 			fmt.Println("Alert!!!")
+			break
 		}
 	}
 }
