024c075682e5dadcc9740067582b10b8c796daaa
eabbaali <elhanafiabbaali@gmail.com>
2026-08-26 11:18:04 +0100

    second

--- concatparams.go
+++ concatparams.go
@@ -1,12 +1,12 @@
 package piscine
 
 func ConcatParams(args []string) string {
-	res := ""//n
-	for s := range args { //args[10.20.30.40.50]
-		res = res + args[s]	
+	res := ""             // n
+	for s := range args { // args[10.20.30.40.50]
+		res = res + args[s]
 		if s != len(args)-1 {
 			res = res + string('\n')
-		}	
+		}
 	}
 	return res
 }
