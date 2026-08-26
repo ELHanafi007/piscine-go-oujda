c43cf518c0ae5dd229532befd1cd77a2067d49a7
eabbaali <elhanafiabbaali@gmail.com>
2026-08-26 21:13:19 +0100

    feels like ultimatepointone?

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	ta := ***a
+	tb := *b
+	tc := *******c
+	td := ****d
+
+	***a = tb
+	*b = td
+	*******c = ta
+	****d = tc
+}
