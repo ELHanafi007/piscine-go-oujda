f9045b756f2b7f4022073520c0dd431fbc2c953c
eabbaali <elhanafiabbaali@gmail.com>
2026-08-26 15:57:43 +0100

    in the last minute

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,32 +1,16 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	list := make(map[string]int)
-	currW := ""
-
-	for _, r := range str {
-		if isalpha(r) {
-			currW += string(r)
-		} else if r == ' ' {
-			if currW != "" {
-				list[currW]++
-				currW = ""
-			}
+	mymap := make(map[string]int)
+	word := ""
+	for _, i := range str {
+		if i == ' ' {
+			mymap[word]++
+			word = ""
+		} else {
+			word += string(i)
 		}
 	}
-
-	if currW != "" {
-		list[currW]++
-	}
-
-	return list
-}
-
-func isalpha(r rune) bool {
-	if r >= 'a' && r <= 'z' {
-		return true
-	} else if r >= 'A' && r <= 'Z' {
-		return true
-	}
-	return false
+	mymap[word]++
+	return mymap
 }
