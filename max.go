85d6233051de16ad6b524b66af0d2f6861124896
eabbaali <elhanafiabbaali@gmail.com>
2026-08-27 07:24:05 +0100

    fish and sh.. sory rock and roll

--- max.go
+++ max.go
@@ -1,11 +1,11 @@
 package piscine
 
 func Max(a []int) int {
-  maksi := a[0]
-  for i := 1; i < len(a);i++{
-    if a[i] > maksi{
-      maksi = a[i]
-    }
-  }
-  return maksi
+	maksi := a[0]
+	for i := 1; i < len(a); i++ {
+		if a[i] > maksi {
+			maksi = a[i]
+		}
+	}
+	return maksi
 }
