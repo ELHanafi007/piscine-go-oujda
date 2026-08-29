41018e31704e30ca42a86a36d73309a7b5bd3b12
eabbaali <elhanafiabbaali@gmail.com>
2026-08-29 07:45:02 +0100

    sorting a slice of intsssss

--- issorted.go
+++ issorted.go
@@ -1,10 +1,20 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) > 0 {
+	if len(a) < 2 {
+		return true
+	}
+
+	direction := f(a[0], a[1])
+
+	for i := 1; i < len(a)-1; i++ {
+		if direction > 0 && f(a[i], a[i+1]) < 0 {
+			return false
+		}
+		if direction < 0 && f(a[i], a[i+1]) > 0 {
 			return false
 		}
 	}
+
 	return true
 }
